33c4e95b4dec209e3c83981535f39c4204e50a01
AndreySamoylenko <100004207+AndreySamoylenko@users.noreply.github.com>
2022-12-04 14:29:34 +1000

    Add files via upload

---
+++ E_drive/main.py
@@ -0,0 +1,145 @@
+from pyb import delay, Pin, ADC, Timer,millis
+from blocks import *
+
+def led(col):
+    if col == 'blue':
+        BLUE.on()
+    else:
+        if col == 'yellow':
+            YELLOW.on()
+        elif col == 'green':
+            GREEN.on()
+
+#calibration()
+f = open('calibration.txt', 'r')
+for i in range(4):
+    Min[i] = int(f.readline())
+for i in range(4):
+    Max[i] = int(f.readline())
+f.close()
+
+
+
+def main():
+    col1 = ''
+    col2 = ''
+    cub_back = ''
+    cub_forward = ''
+    #colol()
+    #pr()
+    YELLOW.on()
+
+    while p_in.value() == 1:
+        delay(1)
+
+    start()
+    cub_forward='black'
+
+    turn(50)
+    turn(50)
+
+    col1 = scan(-1, 1, 'blue')
+    led(col1)
+    print(col1)
+    if col1=='blue':
+        cub_back=col1
+
+    turn(50)
+    long_road(-1)
+
+    if cub_back=='':
+        col2 = scan(-1, 0, 'blue')
+        led(col2)
+        print(col2)
+        if col2=='blue':
+            cub_back=col2
+
+    turn(-50)
+    sbor_l()
+    cub_forward=''
+    turn(50)
+
+    if cub_back=='blue':
+        col2 = scan(1, 0, 'green')
+        led(col2)
+        print(col2)
+        if col2=='green':
+            cub_forward='green'
+
+    if col2=='green' and cub_forward=='':
+        col2 = scan(1, 0, 'any')
+        led(col2)
+        print(col2)
+        cub_forward=col2
+
+    long_road(-1)
+    ms.stop()
+    if col1=='green':
+        turn(-50)
+        pid_x_f(50, 0.5, 0.1, 3)
+        cub_f.angle(-90)
+        cub_forward='green'
+        pid_x_b(50, 0.5, 0.1, 3)
+        turn(50)
+
+    long_road(-1)
+
+    if cub_back=='':
+        col3=scan(-1,0,'any')
+        turn(-50)
+        sbor_r()
+        turn(50)
+        turn(50)
+    elif cub_back=='blue' and cub_forward=='':
+        turn(-50)
+        sbor_r()
+        turn(50)
+        col3=scan(1,0,'any')
+        turn(50)
+    elif cub_back=='blue' and cub_forward=='green':
+        turn(-50)
+        sbor_r()
+        turn(50)
+        turn(50)
+
+
+    sbor_l()
+
+    # if col1=='yellow':
+    #     turn(50)
+    #     long_road(-1)
+    #     turn(50)
+    #     pid_x_b(50, 0.5, 0.1, 3)
+    #     cub_b.angle(-90)
+    #     pid_x_f(50, 0.5, 0.1, 3)
+    #     turn(-50)
+    #     long_road(-1)
+    #     turn(-50)
+    #     sbor_r()
+    #     turn(50)
+    #     long_road(1)
+    #     turn(50)
+    #
+    # elif col2=='yellow':
+    #
+    # else:
+
+
+
+def colol():
+    while 1:
+        print(colour(100))
+def pr():
+    while 1 :
+        print(sens.dat(1),sens.dat(2),sens.dat(3),sens.dat(4),sens.x(),sens.pre_x())
+        delay(10)
+print("starting ---> SUCCESSFUL")
+
+# if b.value() == 0:
+#calibration()
+# else:
+main()
+
+
+
+
---
+++ E_drive/motors.py
@@ -0,0 +1,61 @@
+
+from pyb import delay, Pin, ADC, Timer
+import pyb
+
+class motor:
+    def __init__(self,A1,A2,PWMA,B1,B2,PWMB):
+        self.a1=A1
+        self.a2=A2
+        self.pwma = PWMA
+
+        self.b1 = B1
+        self.b2 = B2
+        self.pwmb = PWMB
+
+        self.Ma1 = Pin(self.a1, Pin.OUT_PP)
+        self.Ma2 = Pin(self.a2, Pin.OUT_PP)
+
+        self.Mb1 = Pin(self.b2, Pin.OUT_PP)
+        self.Mb2 = Pin(self.b1, Pin.OUT_PP)
+
+        self.Spa = Pin(self.pwma)
+        self.tim1 = Timer(12, freq=10000)
+        self.ch1 = self.tim1.channel(1, Timer.PWM, pin=self.Spa)  # пины для работы с драйвером
+
+        self.Spb = Pin(self.pwmb)
+        self.tim = Timer(12, freq=10000)
+        self.ch2 = self.tim.channel(2, Timer.PWM, pin=self.Spb)
+    def drive(self,spA,spB):
+        if spA > 100:
+            spA = 100
+        if spA < -100:
+            spA = -100
+
+        if spA > 0:
+            self.Ma1.low()
+            self.Ma2.high()
+            self.ch1.pulse_width_percent(spA)
+        else:
+            self.Ma2.low()
+            self.Ma1.high()
+            self.ch1.pulse_width_percent(-spA)
+
+        if  spB>100:
+            spB=100
+        if spB <-100:
+            spB =-100
+
+        if spB > 0:
+            self.Mb1.low()
+            self.Mb2.high()
+            self.ch2.pulse_width_percent(spB)
+        else:
+            self.Mb2.low()
+            self.Mb1.high()
+            self.ch2.pulse_width_percent(-spB)
+    def stop(self):
+        self.Ma1.high()
+        self.Mb1.high()
+        self.Ma2.high()
+        self.Mb2.high()
+        pyb.delay(150)
